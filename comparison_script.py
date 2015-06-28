df5816cfa305579f59b493b064f50c903d129317
Yujie He <he72@purdue.edu>
2015-06-28 02:52:18 -0400

    added plots for zstar and csurf

--- comparison_script.py
+++ comparison_script.py
@@ -8,10 +8,13 @@ Created on Wed Jun 24 10:44:19 2015
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import myplot as myplt
+import pandas as pd
 
 expnpzfiles = np.load('out_exp.npz')
 lmnpzfiles  = np.load('out_lm.npz')
-#%% plot hist of exp and lm
+#%% statistics
+#% plot hist of exp and lm
 expr2   = expnpzfiles['out_stat_exp'][:,0]
 exprmse = expnpzfiles['out_stat_exp'][:,1]
 exppcte = expnpzfiles['out_stat_exp'][:,2]
@@ -19,6 +22,7 @@ lmr2   = lmnpzfiles['out_stat_lm'][:,0]
 lmrmse = lmnpzfiles['out_stat_lm'][:,1]
 lmpcte = lmnpzfiles['out_stat_lm'][:,2]
 
+# plot historgram of all profiles that are fitted using exp vs. lm
 expr2cl = filter(lambda x:~np.isinf(x) and ~np.isnan(x) and x<1. and x>0., expr2)
 lmr2cl = filter(lambda x:~np.isinf(x) and ~np.isnan(x) and x<1. and x>0., lmr2)
 
@@ -27,3 +31,37 @@ plt.hist(expr2cl,bins=20,normed=1,alpha=0.5, cumulative=True,label='exp fit (N =
 plt.gca().set_xlabel('r2')
 plt.gca().set_ylabel('cumulative Fraction (%)')
 plt.legend(loc=0)   
+
+#%% properties. z* etc.
+# plot z* using exp vs. lm
+expprofid  = expnpzfiles['out_prop_exp'][:,0]
+expzstar   = expnpzfiles['out_prop_exp'][:,1]
+expcsurf   = expnpzfiles['out_prop_exp'][:,2]
+lmprofid    = lmnpzfiles['out_prop_lm'][:,0]
+lmzstar    = lmnpzfiles['out_prop_lm'][:,1]
+lmcsurf    = lmnpzfiles['out_prop_lm'][:,2]
+
+#  that are fitted using exp vs. lm
+expzstarcl = filter(lambda x:~np.isinf(x) and ~np.isnan(x), expzstar)
+lmzstarcl = filter(lambda x:~np.isinf(x) and ~np.isnan(x), lmzstar)
+lm_df = pd.DataFrame(data=expnpzfiles['out_prop_exp'][:,1:],
+                     index=expnpzfiles['out_prop_exp'][:,0],columns=['zstar','csurf'])
+exp_df = pd.DataFrame(data=lmnpzfiles['out_prop_lm'][:,1:],
+                     index=lmnpzfiles['out_prop_lm'][:,0],columns=['zstar','csurf'])
+join_df = exp_df.join(lm_df, how='inner', lsuffix='_exp', rsuffix='_lm')
+
+fig, axes = plt.subplots(figsize=(10,8))
+axes.scatter(join_df['zstar_exp'], join_df['zstar_lm'])
+axes.set_ylim([-500,1000])
+axes.set_xlim([-500,1000])
+axes.set_xlabel('zstar (cm, exp)')
+axes.set_ylabel('zstar (cm, lm)')
+myplt.refline()
+
+fig, axes = plt.subplots(figsize=(8,6))
+axes.scatter(join_df['csurf_exp'], join_df['csurf_lm'])
+axes.set_ylim([0,60])
+axes.set_xlim([0,60])
+axes.set_xlabel('csurf (%, exp)')
+axes.set_ylabel('csurf (%, lm)')
+myplt.refline()
